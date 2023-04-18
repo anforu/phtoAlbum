9b37e9396aa5498587411e21fc469ae286caa23a
Anforu <anyelo.fonseca@razorfish.com>
2023-04-18 16:51:55 -0600

    NavBar component created

--- photo-album/src/App.js
+++ photo-album/src/App.js
@@ -2,12 +2,15 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/HomePage/HomePage"
 import Login from "./pages/Login/Login"
+import NavBar from './components/NavBar/NavBar'
+
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
-        <Route path="/" element={ <Login/> } />
-        <Route path="home" element={ <Home/> } />
+        <Route path="/" element={<Login />} />
+        <Route path="home" element={<Home />} />
       </Routes>
     </div>
   )
---
+++ photo-album/src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import './NavBar'
+
+export default function Navbar() {
+    return(
+        <nav className='navbar'>
+            <ul>
+                <li className='title'>My NavBar</li>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/">Login</Link></li>
+                <li><Link to="/signup">Signup</Link></li>
+            </ul>
+        </nav>
+    )
+}
