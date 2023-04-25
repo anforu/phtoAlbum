49ce280a1486016053707d6355f7db18b3a35424
Anyelo Fonseca <anyelo.fonseca@razorfish.com>
2023-04-25 12:10:19 -0600

    Photo gallery component working

--- photo-album/src/pages/HomePage/HomePage.js
+++ photo-album/src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Footer from '../../components/Footer/Footer';
+import PhotoAlbum from "react-photo-album";
 import Pet from '../../assets/pets.jpeg';
 import Family from '../../assets/family.jpeg';
 import './HomePage.css';
@@ -9,9 +10,15 @@ const HomePage = () => {
         console.log('Handler to open folder ');
     }
 
+    const photos = [
+        { src: "/images/family.jpeg", width: 800, height: 600 },
+        { src: "/images/pets.jpeg", width: 800, height: 600 },
+        { src: "/images/lion.jpg", width: 800, height: 600 },
+        
+    ];
     return (
         <div className="homepage">
-            <header>My albums</header>
+            <header className="header">My albums</header>
             <div className="container" 
             // onClick={()=> openFolder()}
             >
@@ -31,6 +38,10 @@ const HomePage = () => {
                     <label>New Album</label>
                     <img src={Family} alt="photo" width="100" height="100"/>
                 </div>
+                
+            </div>
+            <div className="photos">
+                <PhotoAlbum  layout="rows" photos={photos} />
             </div>
             <Footer />
         </div>
